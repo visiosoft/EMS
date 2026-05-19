89378f3fc61a010aa41a5ee0923d4a8407f49489
sufyan-nku-dev <safyan.ashraf@nkutechnologies.com>
2026-05-19 20:57:35 +0500

    Wire directory users into internal news

--- backend/src/internal-news/internal-news.module.ts
+++ backend/src/internal-news/internal-news.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AdminUsersModule } from '../admin-users/admin-users.module';
 import { InternalNewsController } from './internal-news.controller';
 import { InternalNewsService } from './internal-news.service';
 
 @Module({
+  imports: [AdminUsersModule],
   controllers: [InternalNewsController],
   providers: [InternalNewsService],
   exports: [InternalNewsService],
