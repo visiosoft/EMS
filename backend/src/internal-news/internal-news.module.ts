0149440ae0cf3ed7ea9012ab5a2457efeec037e8
sufyan-nku-dev <safyan.ashraf@nkutechnologies.com>
2026-05-19 20:56:47 +0500

    Simplify internal news module

--- backend/src/internal-news/internal-news.module.ts
+++ backend/src/internal-news/internal-news.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { News } from '../entities/news.entity';
 import { InternalNewsController } from './internal-news.controller';
 import { InternalNewsService } from './internal-news.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([News])],
   controllers: [InternalNewsController],
   providers: [InternalNewsService],
   exports: [InternalNewsService],
